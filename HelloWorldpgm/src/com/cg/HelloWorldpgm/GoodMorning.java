18056063fded64f814caa2d67ae182f9ad9fb4f0
HP <HP@DESKTOP-9AO4RKT>
2022-06-23 19:02:37 +0530

    first commit

---
+++ HelloWorldpgm/src/com/cg/HelloWorldpgm/GoodMorning.java
@@ -0,0 +1,11 @@
+package com.cg.HelloWorldpgm;
+
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		System.out.println("good morning people");
+		// TODO Auto-generated method stub
+
+	}
+
+}
