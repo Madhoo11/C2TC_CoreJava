a8f19529fa29477d41bdd27fecff9ec56c58ea03
HP <HP@DESKTOP-9AO4RKT>
2022-06-25 15:12:18 +0530

    first commit

---
+++ Helloworld/src/com/cg/Helloworld/Firstpgm.java
@@ -0,0 +1,11 @@
+package com.cg.Helloworld;
+
+public class Firstpgm {
+
+	public static void main(String[] args) {
+		System.out.println("hello java world ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
