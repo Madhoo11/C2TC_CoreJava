7ce2a2e55bf76cc3f45277e18f43841832a10e0e
HP <HP@DESKTOP-9AO4RKT>
2022-06-25 17:31:10 +0530

    first commit

---
+++ Operators/src/com/cg/operators/Pre_and_Post_increment.java
@@ -0,0 +1,67 @@
+package com.cg.operators;
+
+public class Pre_and_Post_increment {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+		int c;
+	    int d;
+		c=++b;//line a
+		d=a++;//line b
+		c++;//line c
+		System.out.println("a="+a + "b="+b +"c="+c +"d="+d);
+		
+		
+		//int a=5,b=18;
+		//if((a=3)==b) {
+		//	System.out.println(++a);
+		//}
+		//else  
+		//{
+			//System.out.println(++b);
+		//}
+//}
+//}
+	
+		
+		//int var1=5;
+		//int var2=6;
+		//if((var2=1)==var1)
+			//System.out.println(var2);
+		//else
+			//System.out.println(++var2);
+	//
+		
+	
+	
+	//int a =2;
+	//int b=5;
+	//int min =(a<b)?a:b;
+	//System.out.println(min);
+		
+		
+		
+		//System.out.print("Y"+"O");
+		//System.out.print('L'+'O');
+		
+		
+		
+		//System.out.print("Y"+ "O");
+		//System.out.print('L');
+		//System.out.print('O');
+		
+		
+		
+		
+		
+		
+		//float f=(1/4)*10;//decimals
+		//int i =(int)f;//type casting
+		//System.out.println(i);
+		
+	}
+}
+		
+		
+	
