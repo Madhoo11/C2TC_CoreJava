bd250a74b6922a8cdcb0f0fb73ff9e86f495ad5e
HP <HP@DESKTOP-9AO4RKT>
2022-06-30 17:57:30 +0530

    first commit

---
+++ DataTypes/src/com/cg/datatypes/CastingExample.java
@@ -0,0 +1,35 @@
+package com.cg.datatypes;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		//int i =010;//octal representation
+		//int j=07;
+		//System.out.println(i);//0*8'0 + 1*8'1 + 0*8'0 = 8
+		//System.out.println(j);//0*8'1 + 7*8'0 = 7   
+		
+		
+		
+		//String name = "John";
+		//System.out.println(name);
+		
+		
+		//int myNum=9;
+		//System.out.println(myNum);
+		
+	
+		//String name="madhoo";
+		//System.out.println("hello"  +  name);
+		
+		//WIDENING CASTING
+		int myInt=9;
+		double myDouble = myInt;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+		
+		
+		
+		
+		
+				}
+}
---
+++ DataTypes/src/com/cg/datatypes/NarrowCastingExample.java
@@ -0,0 +1,56 @@
+package com.cg.datatypes;
+
+public class NarrowCastingExample {
+
+	public static void main(String[] args) {
+		//double myDouble = 9.78;
+        // int myInt = (int)myDouble;//casting done manually
+          //System.out.println(myDouble);
+          //System.out.println(myInt);
+		
+		
+		
+		//char ch = 'c';
+        //int num = 88;
+        //ch = num;//output=error
+		
+		
+		 //float f = 10.5f;
+         //int a=(int)f;
+        // System.out.println(f);
+         //System.out.println(a);
+		
+		
+		
+		
+		//static void test(float x) {
+        //System.out.print("float");
+
+			//}
+             //static void test(double x) {
+             //System.out.print("double");
+//}
+              //public static void main(String[] args) {
+             // test(99.9);
+	
+	
+	
+	
+	
+	
+	int i = 010;//octal representation
+	int j = 07;
+     System.out.println(i);//0*8'0 + 1*8'1 + 0*8'0 = 8
+      System.out.println(j);//0*8'1 + 7*8'0 = 7 
+
+			}
+         
+         
+         
+
+		
+		
+
+	}
+
+
