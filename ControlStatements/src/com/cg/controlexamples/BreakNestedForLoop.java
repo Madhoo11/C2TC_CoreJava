f50fd344ca919c1d0dc444b5573c8b971ea1d891
HP <HP@DESKTOP-9AO4RKT>
2022-07-11 17:29:15 +0530

    first commit

---
+++ ControlStatements/src/com/cg/controlexamples/BreakNestedForLoop.java
@@ -0,0 +1,29 @@
+package com.cg.controlexamples;
+
+public class BreakNestedForLoop {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=3;i++) //1, 2 , 3
+
+		{
+
+		for(int j=1;j<=3;j++) //1 2
+
+		{
+
+		if(i==2&&j==2){
+
+		break; //forceful termination on inner for loop
+
+		}
+
+		System.out.println(i+" "+j); //11, 12, 13
+
+		//21,
+
+		//31, 32, 33
+	}
+		}
+	}
+
+}
---
+++ ControlStatements/src/com/cg/controlexamples/DoWhileExample.java
@@ -0,0 +1,19 @@
+package com.cg.controlexamples;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		String s = "friends";
+
+		int x = 0;
+
+		do{
+
+		System.out.print(s.charAt(x));
+
+		x++ ;
+
+		}while (x < 2);
+	}
+
+}
